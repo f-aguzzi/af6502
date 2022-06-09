cc8b3e1c22a32d7fe9e3ad425ce1bc88c99b7e13
Federico Aguzzi <federico.aguzzi@icloud.com>
2022-06-09 14:55:22 +0200

    Added CLI arguments

--- README.md
+++ README.md
@@ -1,2 +1,34 @@
 # af6502
 6502 8-bit processor emulator
+
+This emulator is still a work in progress. Most of the instruction set is
+still unimplemented.
+
+## Usage
+
+The emulator shall be operated from the command line.
+
+The command line flags must have space between each other, but their
+arguments are attached. For example, to load a file and activate the
+register status output:
+
+```
+af6502 -input=./program.bin -showstatus
+```
+
+### Command line arguments:
+
+- `cycles=` *(followed by a number, without quotes)*: sets the number of
+  clock cycles to execute
+- `-input=` *(followed by the path, without quotes)*: selects the input
+  binary file
+- `-showstatus`: displays the status of the registers in the terminal,
+  at the end of the execution
+- `-dump=` *(followed by the path, without quotes)*: selects the path for
+  the memory dump file
+- `start=` *(followed by a number, without quotes)*: sets the inital value
+  for the Program Counter register, that is, the starting memory location
+  of the executable. When loading a binary, it will be transcribed to
+  memory starting from this value
+
+Invalid flags are ignored.
--- src/main.cpp
+++ src/main.cpp
@@ -6,14 +6,16 @@
 #include <fstream>
 #include <string>
 #include <vector>
+#include "ctre.hpp"
 
 using byte = std::uint8_t;
 using hword = std::uint16_t;
 using word = std::uint32_t;
 
+static constexpr word MEM_SIZE = 1024 * 64;
+
 struct Memory
 {
-    static constexpr word MEM_SIZE = 1024 * 64;
     std::array<byte, MEM_SIZE> Mem;
 
     void init()
@@ -1476,6 +1478,7 @@ struct CPU
     }
 };
 
+// Load 6502 binary from file
 std::vector<byte> load_program(std::string path)
 {
     std::ifstream fin;
@@ -1489,27 +1492,120 @@ std::vector<byte> load_program(std::string path)
     return std::vector<byte>((std::istreambuf_iterator<char>(fin)), std::istreambuf_iterator<char>());
 }
 
-int main()
+// Dump memory to file
+void dump_exec(std::string path, Memory m)
 {
+    std::ofstream fout;
+    fout.open(path, std::ios::binary);
+    if (fout.fail())
+    {
+        std::printf("Error writing dump file");
+        exit(1);
+    }
 
-    CPU cpu(40);
+    for (int i = 0; i < MEM_SIZE; i++)
+    {
+        fout << m[i];
+    }
+}
 
-    // Load and execute test program:
-    /*
-    INC $00
-    INC $00
-    LDA #$02
-    ADC $00
-    STA $00
-    */
-    std::string filename = "test.o";
-    std::vector<byte> loaded_program = load_program(filename);
-    cpu.memory.WriteProgram(loaded_program, 0xFFC);
+// Store the CLI arguments in here
+struct ProgramFlags
+{
+    word cycles = MEM_SIZE;
+    hword start_point = 0x100;
+    std::string path = "";
+    bool dumpStatus = false;
+    bool dumpMem = false;
+    std::string dumpPath;
+};
 
-    cpu.execute(0xFFC);
-    std::printf("A: %d \n", (int)cpu.A);
-    std::printf("mem(0): %d \n", (int)cpu.memory.Mem[0]);
-    
+int main(int argc, char* argv[])
+{
+    std::vector<std::string> args(argv, argv + argc);
+    ProgramFlags pf;
+
+    // Regex patterns for CLI argument matching 
+    static constexpr auto cycles_rxp = ctll::fixed_string{ "(-cycles=)(\\d*)" };
+    static constexpr auto path_rxp = ctll::fixed_string{ "(-input=)(.*)" };
+    static constexpr auto status_rxp = ctll::fixed_string{ "-showstatus" };
+    static constexpr auto dump_rxp = ctll::fixed_string{ "(-dump=)(.*)" };
+    static constexpr auto start_rxp = ctll::fixed_string{ "(-start=)(\\d*)" };
+
+    // Match CLI arguments
+    for (std::string s: args)
+    {
+        // Number of cycles
+        if (auto m = ctre::match<cycles_rxp>(s))
+        {
+            pf.cycles = m.get<2>();
+        }
+        // 6502 binary path
+        else if (auto m = ctre::match<path_rxp>(s))
+        {
+            pf.path = m.get<2>().to_string();
+        }
+        // Show processor status at end of execution
+        else if (ctre::match<status_rxp>(s))
+        {
+            pf.dumpStatus = true;
+        }
+        // Dump to file
+        else if (auto m = ctre::match<dump_rxp>(s))
+        {
+            pf.dumpMem = true;
+            pf.dumpPath = m.get<2>().to_string();
+        }
+        // PC start point
+        else if (auto m = ctre::match<cycles_rxp>(s))
+        {
+            pf.start_point = m.get<2>();
+        }
+    }
+
+    // Quit if no file specified
+    if (pf.path == "")
+    {
+        printf("No input specified. Quitting...");
+        exit(1);
+    }
+
+    // Create CPU and load program
+    CPU cpu(pf.cycles);
+    std::vector<byte> loaded_program = load_program(pf.path);
+    cpu.memory.WriteProgram(loaded_program, 0x100);
+
+    // Execute
+    cpu.execute(0x100);
+
+    // Output registers at end of execution (if required)
+    if (pf.dumpStatus)
+    {
+        // Main registers
+        std::printf("%s", "---- Registers: ---- \n");
+        std::printf("Accumulator (A): %d \n", (int)cpu.A);
+        std::printf("Index 1 (X): %d \n", (int)cpu.X);
+        std::printf("Index 2 (Y): %d \n", (int)cpu.Y);
+        std::printf("Program Counter (PC): %d \n", (int)cpu.PC);
+        std::printf("Stack Pointer (SP): %d \n", (int)cpu.SP);
+
+        // Status flags
+        std::printf("\n%s", "---- Flags: ---- \n");
+        std::printf("Negative flag (N): %d \n", (int)cpu.N);
+        std::printf("Zero flag (Z): %d \n", (int)cpu.Z);
+        std::printf("Break flag (B): %d \n", (int)cpu.B);
+        std::printf("Carry flag (C): %d \n", (int)cpu.C);
+        std::printf("Interrupt flag (I): %d \n", (int)cpu.I);
+        std::printf("Decimal flag (D): %d \n", (int)cpu.D);
+        std::printf("Overflow flag (V): %d \n", (int)cpu.V);
+    }
+
+
+    // Write memdump file (if required)
+    if (pf.dumpMem)
+    {
+        dump_exec(pf.dumpPath, cpu.memory);
+    }
 
     return 0;
 }
